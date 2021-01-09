eac4bd2aea40c50484ad8508ed4234b799a5ce85
Frank-Oroche <frank.oroche.1998@gmail.com>
2021-01-08 22:20:15 -0500

    version 3.0

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from 'src/app/models/User';
+
+import { GamesService } from '../../services/games.service';
 import { CommunicationService } from '../../services/communication.service';
 
-import { User } from 'src/app/models/User';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profile',
@@ -10,6 +13,8 @@ import { User } from 'src/app/models/User';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  games: any = [];
 
   user: User = {
     int_usercodigo: 0,
@@ -23,10 +28,64 @@ export class ProfileComponent implements OnInit {
     vch_userclave: '',
   };
 
-  constructor(private communicationService: CommunicationService) { }
+  constructor(private gamesService: GamesService, private communicationService: CommunicationService) { }
 
   ngOnInit(): void {
     this.user = this.communicationService.user;
+    this.getGames();
+  }
+
+  getGames() {
+    this.gamesService.getMyGames(this.user.int_usercodigo!).subscribe(
+      res => {
+        this.games = res;
+      },
+      err => console.error(err)
+    );
+  }
+
+  deleteGame(id: string) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success m-2',
+        cancelButton: 'btn btn-danger m-2'
+      },
+      buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+      title: 'Esta seguro de ELIMINAR el Juego?',
+      text: "No podras revertir esta accion!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.gamesService.daleteGame(id).subscribe(
+          res => {
+            console.log(res);
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              'Tu juego fue eliminado :(',
+              'success'
+            )
+            this.getGames();
+          },
+          err => console.error(err)
+        );
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Eliminacion Cancelada',
+          'Tu juego esta a salvo :)',
+          'error'
+        )
+      }
+    })
+    
   }
 
 }
